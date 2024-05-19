ba8f391ac56142ae8c991b2ab03c48df9fd6693b
mrGoonies <gooniesdev@gmail.com>
2024-05-19 14:27:52 -0400

    Base proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require_once("includes/header.php") ?>
+<header>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+    </ul>
+</header>
+
+<main>
+    <h1>Welcome to PHP</h1>
+
+</main>
+
+<footer>
+
+</footer>
+<?php require_once("includes/footer.php") ?>
